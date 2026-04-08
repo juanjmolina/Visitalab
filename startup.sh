b5fd3f9047097c78acafb0f1a13cecc464ce6a0e
juanjmolina <107269849+juanjmolina@users.noreply.github.com>
2026-04-08 15:34:44 -0500

    Delete startup.sh
    r

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Azure App Service startup command
-# Este archivo se configura en: App Service > Configuration > General Settings > Startup Command
-# O Azure lo detecta automáticamente si se llama startup.sh
-
-gunicorn --bind=0.0.0.0:8000 --workers=2 --timeout=120 app:app
